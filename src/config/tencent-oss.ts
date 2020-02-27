6109a16485dbf34653571aac5ca5b6a6525ef7a6
rejiejay <454766952@qq.com>
2020-02-27 22:39:13 +0800

    del tencent oss config

--- src/config/tencent-oss.ts
+++ src/config/tencent-oss.ts
@@ -1,9 +1,9 @@
 export const ossConfig = {
-    origin: 'https://rejiejay-1251940173.cos.ap-guangzhou.myqcloud.com/',
-    copySource: 'rejiejay-1251940173.cos.ap-guangzhou.myqcloud.com/',
-    region: 'ap-guangzhou',
-    bucket: 'rejiejay-1251940173',
-    appId: '1251940173',
-    secretId: 'AKIDZw6DLFgMZZKuFO6zSfdMr8D4j9KGmQeh',
-    secretKey: 'xkIbfU4SgfT4RZVCettgEWbR2OYTBetv'
+    origin: '',
+    copySource: '',
+    region: '',
+    bucket: '',
+    appId: '',
+    secretId: '',
+    secretKey: ''
 }
